c31531a7c21e0497baf57a18ca29e6f4efd8d0d7
Nghi Vu <notzatel@gmail.com>
2024-11-04 13:51:42 -0700

    got renaming and printing to work properly

--- main.py
+++ main.py
@@ -14,6 +14,39 @@ os.mkdir(new_dir)
 if os.path.exists(new_dir2):
     shutil.rmtree(new_dir2)
 os.mkdir(new_dir2)
+import os
+import signal
+import time
+import shutil
 
+done = False
+i = 0
+new_dir = "New Folder"
+new_dir2 = "Second Folder"
+
+# chunk overwrites folders if already existing
+if os.path.exists(new_dir):
+    shutil.rmtree(new_dir)
+os.mkdir(new_dir)
+if os.path.exists(new_dir2):
+    shutil.rmtree(new_dir2)
+os.mkdir(new_dir2)
+
+print(os.listdir('.')) # print current directory
+time.sleep (3)
+new_dir = os.rename(new_dir, "hello") # rename new_dir to "hello"
+print(os.listdir('.')) # print current direc again
+
+
+'''
 while not done:
-    os.listdir('.')
+    print(list_directory)
+    os.rename(new_dir, (new_dir + "_" + str(i)))
+    i += 1
+
+    os.rename (new_dir2, (new_dir2 + "_" + str(i)))
+    i+=1
+
+    time.sleep(5)
+    print(list_directory)
+'''
