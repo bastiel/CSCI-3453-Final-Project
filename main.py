66020f651b0cb06a461a4689d8d6ac9fe60e64a7
Nghi Vu <notzatel@gmail.com>
2024-11-04 14:36:56 -0700

    revised rename loop

--- main.py
+++ main.py
@@ -5,48 +5,24 @@ import shutil
 
 done = False
 i = 0
-new_dir = "New Folder"
-new_dir2 = "Second Folder"
-
-if os.path.exists(new_dir):
-    shutil.rmtree(new_dir)
-os.mkdir(new_dir)
-if os.path.exists(new_dir2):
-    shutil.rmtree(new_dir2)
-os.mkdir(new_dir2)
-import os
-import signal
-import time
-import shutil
-
-done = False
-i = 0
-new_dir = "New Folder"
-new_dir2 = "Second Folder"
+folder1 = "New Folder"
+folder2 = "Second Folder"
 
 # chunk overwrites folders if already existing
-if os.path.exists(new_dir):
-    shutil.rmtree(new_dir)
-os.mkdir(new_dir)
-if os.path.exists(new_dir2):
-    shutil.rmtree(new_dir2)
-os.mkdir(new_dir2)
+if os.path.exists(folder1):
+    shutil.rmtree(folder1)
+os.mkdir(folder1)
+if os.path.exists(folder2):
+    shutil.rmtree(folder2)
+os.mkdir(folder2)
 
-print(os.listdir('.')) # print current directory
-time.sleep (3)
-new_dir = os.rename(new_dir, "hello") # rename new_dir to "hello"
-print(os.listdir('.')) # print current direc again
+folder_list= [folder1, folder2]
 
-
-'''
 while not done:
-    print(list_directory)
-    os.rename(new_dir, (new_dir + "_" + str(i)))
+    print(os.listdir('.')) # print current directory
+    time.sleep (3)
+
+    new_name = f"{folder_list[0]}_{i}"
+    os.rename(folder1, new_name)
+    folder1 = new_name
     i += 1
-
-    os.rename (new_dir2, (new_dir2 + "_" + str(i)))
-    i+=1
-
-    time.sleep(5)
-    print(list_directory)
-'''
