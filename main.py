9c13c13417dc87b5c3f8e08878e7d4aa75d66233
Nghi Vu <notzatel@gmail.com>
2024-11-04 13:36:02 -0700

    experimenting with sys calls

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-print("enter your name: ")
-name = input()
-print("Your name: " + name)
+import os
+import signal
+import time
+import shutil
+
+done = False
+i = 0
+new_dir = "New Folder"
+new_dir2 = "Second Folder"
+
+if os.path.exists(new_dir):
+    shutil.rmtree(new_dir)
+os.mkdir(new_dir)
+if os.path.exists(new_dir2):
+    shutil.rmtree(new_dir2)
+os.mkdir(new_dir2)
+
+while not done:
+    os.listdir('.')
